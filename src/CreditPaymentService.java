d011f6b050b5dd3b4876dfc96e1c69611598e961
dasha T <olga061200@icloud.com>
2023-12-08 11:49:45 +0300

    New version

--- src/CreditPaymentService.java
+++
@@ -1,8 +0,0 @@
-public class CreditPaymentService {
-    public double calculate (int sum, double intRate, int timeMonths) {
-        double payment = (sum * intRate + sum ) / timeMonths;
-        int wholePayment = (int) payment;
-        return wholePayment;
-    }
-}
-// interest rate is always 9.99%, sum 1_000_000, payment x, time x months
