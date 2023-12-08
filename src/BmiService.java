028c5ddf2d37825ed305989642c9573eb58c36f0
dasha T <olga061200@icloud.com>
2023-12-08 12:10:00 +0300

    deleted 2

--- src/BmiService.java
+++
@@ -1,6 +0,0 @@
-public class BmiService {
-    public double calculate (double height_m, double weight_kg) {
-        double index = weight_kg/ Math.pow(height_m,2);
-        return index;
-    }
-}
